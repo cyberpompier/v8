d21cde40d2da49144c4229ba83c59a8c70ed0ba6
cyberpompier <sebastien.dupressoir@icloud.com>
2025-02-04 10:40:06 +0100

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,12 @@ export default defineConfig({
         description: 'A simple PWA labels app',
         icons: [
           {
-            src: '/android-chrome-192x192.png',
+            src: '/pwa-icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: '/pwa-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
